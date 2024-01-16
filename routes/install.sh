d46209e07032b472dcb34748a241a3b6595825e5
Joe Daccache <52009731+daccachejoe@users.noreply.github.com>
2024-01-16 13:44:37 -0500

    Update install.sh

--- routes/install.sh
+++ routes/install.sh
@@ -3,6 +3,7 @@
 # install the required packages to make scmultiome executable in the desired directory
 mkdir .lib
 git clone https://bitbucket.org/djhshih/argparser.git .lib/argparser
+mv scripts/install.R .lib/argparser/
 cd .lib/argparser
 Rscript install.R
 R CMD INSTALL .
