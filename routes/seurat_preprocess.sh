617488f06c06fce54346d439f5b442adf239b15e
Joe Daccache <52009731+daccachejoe@users.noreply.github.com>
2024-04-01 17:08:42 -0400

    conda env add

--- routes/seurat_preprocess.sh
+++ routes/seurat_preprocess.sh
@@ -3,8 +3,9 @@
 echo $my_macs_path
 echo $project_prefix
 # seurat preprocessing chunk
-module load r/4.1.2
-
+# module load r/4.1.2
+pc
+conda activate $conda_env_name
 
 if [ -f "data/raw/souporcell/out_gex/barcode-assignment-df.csv" ]; then
     scripts/multiome-processing.R \
